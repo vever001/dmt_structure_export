a41092626855321d6dbccdcd696136b65e46fee3
Herve Donner <hervedonner@gmail.com>
2019-06-25 17:56:22 +0200

    Update EntityBundlesDataExporter.

--- src/DataExporter/EntityBundlesDataExporter.php
+++ src/DataExporter/EntityBundlesDataExporter.php
@@ -18,6 +18,11 @@ class EntityBundlesDataExporter extends DataExporter implements DataExporterInte
       'entity_count' => dt('Entity count'),
       'bundle' => dt('Bundle'),
       'bundle_count' => dt('Bundle count'),
+      'multilingual_enabled' => dt('Multilingual enabled'),
+      'multilingual_type' => dt('Multilingual type'),
+      'comment_settings' => dt('Comment settings'),
+      'revisions_enabled' => dt('Revisions enabled'),
+      'moderation_enabled' => dt('Workbench moderation enabled'),
     );
   }
 
@@ -28,22 +33,155 @@ class EntityBundlesDataExporter extends DataExporter implements DataExporterInte
     $entity_info = entity_get_info();
 
     foreach ($entity_info as $entity_type => $et_info) {
-      $this->addRow(array(
-        // Entity Label.
+      $has_bundles = !empty($et_info['bundles']) && count($et_info['bundles']) > 1;
+      $entity_row = array(
         'entity' => $et_info['label'] . ' (' . $entity_type . ')',
-        // Entity count.
         'entity_count' => Utilities::getEntityDataCount($entity_type),
-      ));
+      );
 
-      if (!empty($et_info['bundles']) && count($et_info['bundles']) > 1) {
+      if (!$has_bundles) {
+        // Multilingual settings.
+        $multilingual_type = $this->getEntityTranslationType($entity_type);
+        $entity_row['multilingual_enabled'] = $multilingual_type ? 'TRUE' : 'FALSE';
+        $entity_row['multilingual_type'] = $multilingual_type;
+      }
+
+      $this->addRow($entity_row);
+
+      // Process bundles.
+      if ($has_bundles) {
         foreach ($et_info['bundles'] as $bundle => $bundle_info) {
-          $this->addRow(array(
+          $bundle_row = array(
             'bundle' => $bundle_info['label'] . ' (' . $bundle . ')',
             'bundle_count' => Utilities::getEntityDataCount($entity_type, $bundle),
-          ));
+          );
+
+          // Multilingual settings.
+          $multilingual_type = $this->getEntityTranslationType($entity_type, $bundle);
+          $bundle_row['multilingual_enabled'] = $multilingual_type ? 'TRUE' : 'FALSE';
+          $bundle_row['multilingual_type'] = $multilingual_type;
+
+          // Entity type specific process.
+          switch ($entity_type) {
+            case 'node':
+              $bundle_row += $this->processNodeBundleRow($bundle);
+              break;
+          }
+
+          $this->addRow($bundle_row);
         }
       }
     }
+  }
+
+  /**
+   * Process a node bundle row.
+   */
+  protected function processNodeBundleRow($bundle) {
+    $row = array();
+
+    // Comments.
+    if (module_exists('comment')) {
+      $comment_enabled = variable_get('comment_' . $bundle, COMMENT_NODE_OPEN);
+      $comment_enabled_options = array(
+        COMMENT_NODE_OPEN => dt('Open'),
+        COMMENT_NODE_CLOSED => dt('Closed'),
+        COMMENT_NODE_HIDDEN => dt('Hidden'),
+      );
+      $row['comment_settings'] = isset($comment_enabled_options[$comment_enabled]) ? $comment_enabled_options[$comment_enabled] : dt('Unknown');
+    }
+
+    // Revisions.
+    $node_options = variable_get('node_options_' . $bundle, array('status', 'promote'));
+    $row['revisions_enabled'] = in_array('revision', $node_options) ? 'TRUE' : 'FALSE';
+
+    // Moderation.
+    if (module_exists('workbench_moderation')) {
+      $row['moderation_enabled'] = workbench_moderation_node_type_moderated($bundle) ? 'TRUE' : 'FALSE';
+    }
+
+    return $row;
+  }
+
+  /**
+   * Returns the translation type/handler for the given entity type.
+   */
+  protected function getEntityTranslationType($entity_type, $bundle = NULL) {
+    switch ($entity_type) {
+      case 'node':
+        return $this->getEntityTranslationTypeNode($bundle);
+
+      case 'taxonomy_term':
+        return $this->getEntityTranslationTypeTaxonomyTerm($bundle);
+
+      default:
+        return $this->getEntityTranslationTypeEntityTranslation($entity_type, $bundle);
+    }
+  }
+
+  /**
+   * Returns the translation type/handler for a given node bundle.
+   */
+  protected function getEntityTranslationTypeNode($bundle) {
+    $multilingual_type = variable_get('language_content_type_' . $bundle, 0);
+    if ($multilingual_type) {
+      $multilingual_type_options = array(
+        1 => dt('Enabled (no translations)'),
+        TRANSLATION_ENABLED => dt('Enabled, with translation (translation module)'),
+        ENTITY_TRANSLATION_ENABLED => dt('Enabled, with field translation (entity_translation module)'),
+      );
+      return isset($multilingual_type_options[$multilingual_type]) ? $multilingual_type_options[$multilingual_type] : dt('Unknown');
+    }
+  }
+
+  /**
+   * Returns the translation type/handler for a given taxonomy_term bundle.
+   */
+  protected function getEntityTranslationTypeTaxonomyTerm($bundle) {
+    if ($multilingual_type = $this->getEntityTranslationTypeTaxonomyTermI18n($bundle)) {
+      return $multilingual_type;
+    }
+    elseif ($multilingual_type = $this->getEntityTranslationTypeEntityTranslation('taxonomy_term', $bundle)) {
+      return $multilingual_type;
+    }
+  }
+
+  /**
+   * Returns the entity_translation type/handler for a given entity type.
+   */
+  protected function getEntityTranslationTypeEntityTranslation($entity_type, $bundle = NULL) {
+    if (!module_exists('entity_translation') || !entity_translation_enabled($entity_type)) {
+      return FALSE;
+    }
+
+    if (!empty($bundle) && !entity_translation_enabled_bundle($entity_type, $bundle)) {
+      return FALSE;
+    }
+
+    return dt('Enabled, with field translation (entity_translation module)');
+  }
+
+  /**
+   * Returns the i18n translation type for a given vocabulary.
+   */
+  protected function getEntityTranslationTypeTaxonomyTermI18n($vocabulary_name) {
+    if (module_exists('i18n_taxonomy')) {
+      $vocabulary = taxonomy_vocabulary_machine_name_load($vocabulary_name);
+      if ($vocabulary) {
+        $multilingual_type = i18n_taxonomy_vocabulary_mode($vocabulary->vid);
+        if ($multilingual_type) {
+          $multilingual_type_options = [
+            I18N_MODE_LOCALIZE => dt('Localize (i18n_taxonomy module)'),
+            I18N_MODE_TRANSLATE => dt('Translate (i18n_taxonomy module)'),
+            I18N_MODE_MULTIPLE => dt('Translate and Localize (i18n_taxonomy module)'),
+            I18N_MODE_LANGUAGE => dt('Fixed Language (i18n_taxonomy module)'),
+          ];
+          return isset($multilingual_type_options[$multilingual_type]) ? $multilingual_type_options[$multilingual_type] : dt('Unknown');
+        }
+      }
+    }
+
+    return FALSE;
   }
 
 }
