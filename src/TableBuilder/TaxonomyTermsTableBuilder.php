f949cc2db0b8404d4e30c9444e3124ff1cca5588
ioannis-ampatzis <ampatzis.ioannis@gmail.com>
2019-09-24 15:40:39 +0200

    Bug fix: Line breaks are not handled correctly and they break CSV.

--- src/TableBuilder/TaxonomyTermsTableBuilder.php
+++ src/TableBuilder/TaxonomyTermsTableBuilder.php
@@ -34,8 +34,37 @@ class TaxonomyTermsTableBuilder extends TableBuilder {
     $query->orderby('term.vid');
     $query->orderby('term.name');
     $rows = $query->execute()->fetchAllAssoc('tid', \PDO::FETCH_ASSOC);
+    $this->sanitizeDescriptions($rows);
 
     $this->setRows($rows);
+  }
+
+  /**
+   * Sanitizes all term descriptions.
+   *
+   * @param $rows
+   *   The rows of taxonomy terms.
+   */
+  protected function sanitizeDescriptions(&$rows) {
+    foreach ($rows as &$row) {
+      $row['term_description'] = $this->sanitizeDescription(trim($row['term_description']));
+    }
+  }
+
+  /**
+   * Sanitizes a single term description.
+   *
+   * @param $string
+   *   The string to sanitize.
+   *
+   * @return string|string[]|null
+   *   The sanitized string or null.
+   */
+  protected function sanitizeDescription($string) {
+    $patterns = ['/"/', '/\r?\n/'];
+    $replacements = ['""', '<br>'];
+
+    return preg_replace($patterns, $replacements, $string);
   }
 
 }
